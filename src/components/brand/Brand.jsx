8847fc197160f24221e0564a36a9b5c35a91fb8d
Faizan Mustafa <dmis-fa18-012@tuf.edu.pk>
2024-10-17 11:07:33 +0500

    Feature Componets , gpt3 section Compleated

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { google, slack, atlassian, dropbox, shopify } from './import'
 import './brand.css'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt='Google img' srcset='' />
+      </div>
+      <div>
+        <img src={slack} alt='slack img' srcset='' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian img' srcset='' />
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox img' srcset='' />
+      </div>
+      <div>
+        <img src={shopify} alt='shopify img' srcset='' />
+      </div>
+    </div>
   )
 }
 
